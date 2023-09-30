f502f2c35f91f939bb172d432d82dda4011710b0
lqviet45 <lqviet455@gmail.com>
2023-09-30 07:56:49 +0700

    adding test

--- TestDateTimeChecker/Tests.cs
+++ TestDateTimeChecker/Tests.cs
@@ -22,7 +22,17 @@ namespace TestDateTimeChecker
 
 			var actualValue = _dateTimeService.CheckDayInMonth(month, year);
 
-			//Assert.That(actualValue, Is.(ExpectDay));
+			Assert.That(actualValue, Is.Not.EqualTo(ExpectDay));
+		}
+
+		[Test]
+		public void CheckDayInMonth_ShouldReturn28_ToBeSuccess()
+		{
+			var ExpectDay = 28;
+			var month = 2;
+			var year = 2001;
+
+			var actualValue = _dateTimeService.CheckDayInMonth(month, year);
 		}
 	}
 }
