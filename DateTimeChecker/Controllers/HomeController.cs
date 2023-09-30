23a6330af0f18e5fea6154c6b7c5691785843e1a
mkhoi15 <khoivmse171254@fpt.edu.vn>
2023-09-30 08:01:02 +0700

    chang some thing

--- DateTimeChecker/Controllers/HomeController.cs
+++ DateTimeChecker/Controllers/HomeController.cs
@@ -25,76 +25,74 @@ namespace DateTimeChecker.Controllers
         public IActionResult CheckDay(Date date)
         {
             bool IsError = false;
-
+			
+            // Check Day
             if(date.Day == null)
             {
                 IsError = true;
                 ViewBag.DayError = "Day field can not be blank";
-            }   
+            }
 
-            if (date.Month == null)
+			bool IsNumberDay = int.TryParse(date.Day, out int day) ;
+
+
+			if (IsNumberDay == false)
+			{
+				IsError = true;
+				ViewBag.DayError = "Day field must me a number";
+			}
+
+			if (day < 0 || day > 31)
+			{
+				IsError = true;
+				ViewBag.DayError = "Day must be between 1 and 31";
+			}
+
+            // Check Month
+			if (date.Month == null)
             {
                 IsError = true;
                 ViewBag.MonthError = "Month field can not be blank";
             }
 
-            if (date.Year == null)
-            {
-                IsError = true;
-                ViewBag.YearError = "Year field can not be blank";
-            }
+			
+			bool IsNumberMonth = int.TryParse(date.Month, out int month);
 
-            if(IsError)
-            {
-                return View("Index");
-            }
+			if (IsNumberMonth == false)
+			{
+				IsError = true;
+				ViewBag.MonthError = "Month field must me a number";
+			}
 
-            int day = 0;
-            int month = 0;
-            int year = 0;
-            bool IsNumberDay = int.TryParse(date.Day, out day);
-            bool IsNumberMonth = int.TryParse(date.Month, out month);
-            bool IsNumberYear = int.TryParse(date.Year, out year);
-            if(IsNumberDay == false )
-            {
-                IsError = true;
-                ViewBag.DayError = "This field must me a number";      
-            }
-            if (IsNumberMonth == false)
-            {
-                IsError = true;
-                ViewBag.MonthError = "This field must me a number";
-            }
-            if (IsNumberYear == false)
-            {
-                IsError = true;
-                ViewBag.YearError = "This field must me a number";
-            }
-
-            if(IsError)
-            {
-                return View("Index");
-            }
-
-            if(day < 0 || day > 31)
-            {
-                IsError = true;
-                ViewBag.DayError = "Day must be between 1 and 31";
-            }
-
-            if (month < 0 || month > 12)
-            {
+			if (month < 0 || month > 12)
+			{
 				IsError = true;
 				ViewBag.MonthError = "Month must be between 1 and 12";
 			}
 
-            if (year < 1000 || year > 3000)
-            {
-                IsError = true;
-                ViewBag.YearError = "Year must be between 1000 and 3000";
-            }
+			// Check Year
+			if (date.Year == null)
+			{
+				IsError = true;
+				ViewBag.YearError = "Year field can not be blank";
+			}
 
-            if (IsError)
+			
+			bool IsNumberYear = int.TryParse(date.Year, out int year);
+
+			if (IsNumberYear == false)
+			{
+				IsError = true;
+				ViewBag.YearError = "Year field must me a number";
+			}
+
+			if (year < 1000 || year > 3000)
+			{
+				IsError = true;
+				ViewBag.YearError = "Year must be between 1000 and 3000";
+			}
+
+			if (IsError)
             {
                 return View("Index");
             }
